a56933eca9d4beb3ac865214024eca24ad3de51d
bensonz <mr.bz@hotmail.com>
2023-11-05 14:37:00 +0800

    align definitions

--- src/definitions/slider.def.ts
+++ src/definitions/slider.def.ts
@@ -1,6 +1,7 @@
 export interface ISliderImage {
   id: string;
-  link: string;
+  url: string;
+  metaData: Record<string, any>;
 }
 
 export interface ISliderItem {
@@ -9,7 +10,7 @@ export interface ISliderItem {
   description: string;
   buttonText: string;
   component: string;
-  backgroundImage: ISliderImage;
+  bgImage: ISliderImage;
 }
 
 /**
